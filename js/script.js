db0055303b7205e66d7e77399e97e44895510845
artemchiruhin <artemchiruhin@bk.ru>
2020-11-30 18:21:28 +0500

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,132 +1,117 @@
-// Меню
-/*let menuBtn = document.querySelector(".menu-btn");
-let menu = document.querySelector(".menu");
-menuBtn.onclick = function(e){
-  e.preventDefault();
-  menuBtn.classList.toggle("menu-btn_active");
-  menu.classList.toggle("menu_active");
-}*/
-
-
-// Калькулятор
-let text = document.querySelector(".text");
-let calc = document.querySelector(".calculator");
-let bar = document.querySelector(".bar");
-let codeOper;
-let num1;
-let num2;
-calc.onclick = function(event){
-	let target = event.target;
-	if(target.textContent != "<=" && target.textContent != "+" && target.textContent != "=" && target.textContent != "/" && target.textContent != "*" && target.textContent != "-"){
-		text.textContent += target.textContent;
+// Секундомер
+let time = 0;
+let isPaused = false;
+let timerId = setInterval(() => {
+	if(!isPaused){
+		if(time > 59){
+			timer.innerHTML = '01:00';
+		} else if(time < 10){
+			timer.innerHTML = '00:0' + time;
+		} else{
+			timer.innerHTML = '00:' + time;
+		}
+		time++;
 	}
-	if(target.textContent == "<="){
-		text.textContent = text.textContent.slice(0, -1);
-	}
-	if(target.textContent == "/"){
-		codeOper = 1;
-		num1 = +text.textContent;
-		text.textContent = "";
-	}
-	if(target.textContent == "*"){
-		codeOper = 2;
-		num1 = +text.textContent;
-		text.textContent = "";
-	}
-	if(target.textContent == "-"){
-		codeOper = 3;
-		num1 = +text.textContent;
-		text.textContent = "";
-	}
-	if(target.textContent == "+"){
-		codeOper = 4;
-		num1 = +text.textContent;
-		text.textContent = "";
-	}
-	if(target == document.querySelector("#equal")){
-		num2 = +text.textContent;
-		switch(codeOper){
-			case 1:
-				text.textContent = num1 / num2;
-				break;
-			case 2:
-				text.textContent = num1 * num2;
-				break;
-			case 3:
-				text.textContent = num1 - num2;
-				break;
-			case 4:
-				text.textContent = num1 + num2;
-				break;
+}, 1000);
+let cvs = document.querySelector('#canvas'); // Канвас
+let ctx = cvs.getContext('2d'); // Контекст
+// Изображения
+let bg = new Image();
+let basket = new Image();
+let banana = new Image();
+// Пути файлов
+bg.src = "../img/bg.png";
+basket.src = '../img/basket.svg';
+banana.src = '../img/banana.png';
+// Позиция корзины
+let xBasketPos = document.querySelector('.gameArea').width / 2;
+let yBasketPos = document.querySelector('.gameArea').height - 145;
+// Счёт
+let score = document.querySelector('#score');
+// Жизни
+let livesCounter = document.querySelector('#livesCounter');
+// Массив фруктов
+let fruits = [];
+fruits[0] = {
+	x: Math.floor(Math.random() * 800),
+	y: 20,
+	
+};
+// Нужна, чтобы работала пауза
+let animationGame;
+// Отрисовка изображений
+function draw(){
+	ctx.drawImage(bg, 0, 0, 800, 600);
+	// Отрисовка фруктов
+	for(let i = 0; i < fruits.length; i++){
+		ctx.drawImage(banana, fruits[i].x, fruits[i].y, 25, 25);
+		//fruits[i].y += fruits[i].speed; // Падение фрукта
+		fruits[i].y++;
+		// Проверка на падение на землю
+		if(fruits[i].y >= 470){
+			fruits[i].y = 20; // Возвращение на исходную
+			fruits[i].x = Math.floor(Math.random() * 800); // Рандомное место по Х
+			livesCounter.textContent--; // Вычитание жизни
+		}
+		// Проверка на попадание в корзину
+		if(fruits[i].x >= xBasketPos && fruits[i].x + 25 <= xBasketPos + 45 && fruits[i].y > yBasketPos){
+			score.textContent++; // Увеличение счета
+			fruits[i].y = 20; // Возвращение на исходную
+			fruits[i].x = Math.floor(Math.random() * 800); // Рандомное место по Х
 		}
 	}
-}
-setInterval(function(){
-	bar.classList.toggle("none");
-}, 500);
-
-// Случайный цвет
-
-let str = "0123456789ABCDEF";
-let res = "";
-let btn = document.querySelector(".button");
-let textColor = document.querySelector(".textColor");
-let bg = document.querySelector(".randomColor");
-btn.onclick = function(){
-	for(let i = 0; i < 6; i++){
-		let random = Math.floor(Math.random() * 15);
-		res += str[random];
+	ctx.drawImage(basket, xBasketPos, yBasketPos, 45, 45);
+	// Проверка на победу/поражение
+	if(livesCounter.textContent === '0' && time > 10){
+		// Переход на страницу с результатами, плюс get параметры для php
+		window.location = `../pages/results.php?score=` + score.textContent + `&time=` + timer.textContent + `&is_winner=` + true;
+	} else if(livesCounter.textContent <= '0' && time < 10){
+		window.location = `../pages/results.php?score=` + score.textContent + `&time=` + timer.textContent + `&is_winner=` + false;
 	}
-	bg.style.background = "#" + res;
-	textColor.style.color = "#" + res;
-	textColor.textContent = "#" + res;
-	btn.style.color = "#" + res;
-	btn.style.borderColor = "#" + res;
-	res = "";
-};
-
-// Scroll
-function scrollTo(element) {
-	window.scroll({
-		left: 0,
-		top: element.offsetTop,
-		behavior: 'smooth'
-	})
+	animationGame = requestAnimationFrame(draw);
 }
-let animatedBg = document.querySelector('#animatedBackground-link');
-animatedBg.addEventListener('click', () => {
-	scrollTo(document.querySelector('.animatedBackground'));
-});
-let calculatorLink = document.querySelector('#calculator-link');
-calculatorLink.addEventListener('click', () => {
-	scrollTo(document.querySelector('.calculatorSection'));
-});
-let randomColorLink = document.querySelector('#randomColor-link');
-randomColorLink.addEventListener('click', () => {
-	scrollTo(document.querySelector('.randomColor'));
-});
-let arrowUp = document.querySelector('.arrow-up');
-arrowUp.addEventListener('click', () => {
-	scrollTo(document.querySelector('header'));
-});
-/*let arrow = document.querySelector('.arrow');
-arrow.addEventListener('click', () => {
-	scrollTo(document.querySelector('.animatedBackground'));
-});*/
-
-window.addEventListener('scroll', () => {
-	if(window.pageYOffset === 1000){
-		arrowUp.style.opacity = '1';
-		gsap.timeline().from('.arrow-up', 0.5, {x: -300});
+// Отрисовка происходит, когда последнее изображение загрузится
+banana.onload = draw;
+// Движение корзины и пауза
+document.addEventListener('keydown', e => {
+	if(e.keyCode == 37) {
+		if(xBasketPos > 0){
+			xBasketPos -= 12.5;
+		}
 	}
-	if(window.pageYOffset < 1000){
-		arrowUp.style.opacity = '0';
+	if(e.keyCode == 39) {
+		if(xBasketPos < 755){
+			xBasketPos += 12.5;
+		}
+	}
+	if(e.keyCode == 27){
+		if(!isPaused){
+			cancelAnimationFrame(animationGame);
+			isPaused = true;
+		} else if(isPaused){
+			requestAnimationFrame(draw);
+			isPaused = false;
+		}
+	}
+});
+// Тут должна быть фича с пробелом
+/*document.addEventListener('keypress', e => {
+	if(e.keyCode == 32){
+		ultimate = setTimeout(() => {
+			console.log("Работает");
+		}, 4000);
 	}
 })
-// Анимация
-gsap.timeline()
-.fromTo('.logo', 1, {opacity: 0, x: -300}, {opacity: 1, x: 0})
-.fromTo('.menu', 1, {y: -120}, {y: 0}, '-=1')
-.fromTo('.phone', 1, {x: 300}, {x: 0}, '-=1')
-.fromTo('.leftSide', 1, {opacity: 0, x: -300}, {opacity: 1, x: 0})
-.fromTo('.rightSide', 1, {transform: 'scale(0)'}, {transform: 'scale(1)'});
+document.addEventListener('keyup', e => {
+	if(e.keyCode == 32){
+		clearInterval(ultimate);
+	}
+})*/
+// Добавление фрукта каждую секунду
+setInterval(() => {
+	fruits.push({
+		x: Math.floor(Math.random() * 400),
+		y: 20,
+		speed: Math.random() * 2 + 1
+	});
+}, 1000);
