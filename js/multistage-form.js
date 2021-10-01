28a466ac8c401934d2e889983e008cb347997d9e
artemchiruhin <artemchiruhin@bk.ru>
2021-10-01 16:00:22 +0500

    updated

--- js/multistage-form.js
+++ js/multistage-form.js
@@ -4,34 +4,51 @@ const toThirdFromSecondBtn = document.querySelector('.to_third_from_second');
 const toSecondFromThirdBtn = document.querySelector('.to_second_from_third');
 const toFirstStageBtn = document.querySelector('.to_first_stage');
 
+const tabs = document.querySelectorAll('.tab');
+
 const blocks = document.querySelectorAll('.form_block');
 
 toSecondFromFirstBtn.addEventListener('click', e => {
 	e.preventDefault();
-	blocks.forEach(block => block.classList.add('closed'));
-	blocks[1].classList.remove('closed');
+	setActiveBlock(1);
+	setActiveTab(1);
 });
 
 toFirstFromSecondBtn.addEventListener('click', e => {
 	e.preventDefault();
-	blocks.forEach(block => block.classList.add('closed'));
-	blocks[0].classList.remove('closed');
+	setActiveBlock(0);
+	setActiveTab(0);
 });
 
 toThirdFromSecondBtn.addEventListener('click', e => {
 	e.preventDefault();
-	blocks.forEach(block => block.classList.add('closed'));
-	blocks[2].classList.remove('closed');
+	setActiveBlock(2);
+	setActiveTab(2);
 });
 
 toSecondFromThirdBtn.addEventListener('click', e => {
 	e.preventDefault();
-	blocks.forEach(block => block.classList.add('closed'));
-	blocks[1].classList.remove('closed');
+	setActiveBlock(1);
+	setActiveTab(1);
 });
 
 toFirstStageBtn.addEventListener('click', e => {
 	e.preventDefault();
-	blocks.forEach(block => block.classList.add('closed'));
-	blocks[0].classList.remove('closed');
+	setActiveBlock(0);
+	setActiveTab(0);
 });
+
+tabs.forEach((tab, i) => tab.addEventListener('click', () => {
+	setActiveTab(i);
+	setActiveBlock(i);
+}));
+
+const setActiveBlock = index => {
+	blocks.forEach(block => block.classList.remove('active_block'));
+	blocks[index].classList.add('active_block');
+}
+
+const setActiveTab = index => {
+	tabs.forEach(t => t.classList.remove('active_tab'));
+	tabs[index].classList.add('active_tab');
+}
